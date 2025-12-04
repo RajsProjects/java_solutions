2a5845966b6a5f4fb396bea83933c800d3132dfd
Aache_wale_bhaiya <sumanmandal8955@gmail.com>
2025-12-04 20:37:52 +0530

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,36 @@
+package java_soloutions;
+
+
+
+
+public class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return BuildBst(nums, 0, nums.length -1);
+    }
+
+    public TreeNode BuildBst(int[] nums, int left, int right){
+        if(left > right){
+            return null;
+        }
+
+        int mid = (left + right)/2;
+ TreeNode root = new TreeNode(nums[mid]);
+        root.left = BuildBst(nums, left, mid - 1);
+        root.right = BuildBst(nums, mid + 1, right);
+
+        return root;
+    }
+}
